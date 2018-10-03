ed50bbf1303ac29c8af1d2e53e9db8ebf9b80524
Michal Fojtik <mfojtik@redhat.com>
2018-10-03 15:04:07 +0200

    ensure operator config exists on start

--- pkg/operator/starter.go
+++ pkg/operator/starter.go
@@ -2,16 +2,24 @@ package operator
 
 import (
 	"fmt"
+	"os"
 	"time"
 
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
 	kubeinformers "k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	apiregistrationclient "k8s.io/kube-aggregator/pkg/client/clientset_generated/clientset"
 	apiregistrationinformers "k8s.io/kube-aggregator/pkg/client/informers/externalversions"
 
+	"github.com/openshift/cluster-openshift-apiserver-operator/pkg/apis/openshiftapiserver/v1alpha1"
 	operatorconfigclient "github.com/openshift/cluster-openshift-apiserver-operator/pkg/generated/clientset/versioned"
+	operatorsv1alpha1client "github.com/openshift/cluster-openshift-apiserver-operator/pkg/generated/clientset/versioned/typed/openshiftapiserver/v1alpha1"
 	operatorclientinformers "github.com/openshift/cluster-openshift-apiserver-operator/pkg/generated/informers/externalversions"
+	"github.com/openshift/cluster-openshift-apiserver-operator/pkg/operator/v311_00_assets"
 )
 
 func RunOperator(clientConfig *rest.Config, stopCh <-chan struct{}) error {
@@ -42,6 +50,9 @@ func RunOperator(clientConfig *rest.Config, stopCh <-chan struct{}) error {
 		kubeClient,
 		apiregistrationv1Client.ApiregistrationV1(),
 	)
+
+	ensureOperatorConfigExists(operator.operatorConfigClient, "v3.11.0/openshift-apiserver/operator-config.yaml")
+
 	configObserver := NewConfigObserver(
 		operatorConfigInformers.Openshiftapiserver().V1alpha1().OpenShiftAPIServerOperatorConfigs(),
 		kubeInformersLocallyNamespaced,
@@ -58,4 +69,48 @@ func RunOperator(clientConfig *rest.Config, stopCh <-chan struct{}) error {
 
 	<-stopCh
 	return fmt.Errorf("stopped")
+}
+
+func ensureOperatorConfigExists(client operatorsv1alpha1client.OpenShiftAPIServerOperatorConfigsGetter, path string) {
+	v1alpha1Scheme := runtime.NewScheme()
+	v1alpha1.Install(v1alpha1Scheme)
+	v1alpha1Codecs := serializer.NewCodecFactory(v1alpha1Scheme)
+	operatorConfigBytes := v311_00_assets.MustAsset(path)
+	operatorConfigObj, err := runtime.Decode(v1alpha1Codecs.UniversalDecoder(v1alpha1.GroupVersion), operatorConfigBytes)
+	if err != nil {
+		panic(err)
+	}
+	requiredOperatorConfig, ok := operatorConfigObj.(*v1alpha1.OpenShiftAPIServerOperatorConfig)
+	if !ok {
+		panic(fmt.Sprintf("unexpected object in %s: %t", path, operatorConfigObj))
+	}
+
+	hasImageEnvVar := false
+	if imagePullSpecFromEnv := os.Getenv("IMAGE"); len(imagePullSpecFromEnv) > 0 {
+		hasImageEnvVar = true
+		requiredOperatorConfig.Spec.ImagePullSpec = imagePullSpecFromEnv
+	}
+
+	existing, err := client.OpenShiftAPIServerOperatorConfigs().Get(requiredOperatorConfig.Name, metav1.GetOptions{})
+	if errors.IsNotFound(err) {
+		if _, err := client.OpenShiftAPIServerOperatorConfigs().Create(requiredOperatorConfig); err != nil {
+			panic(err)
+		}
+		return
+	}
+	if err != nil {
+		panic(err)
+	}
+
+	if !hasImageEnvVar {
+		return
+	}
+
+	// If ImagePullSpec changed, update the existing config instance
+	if existing.Spec.ImagePullSpec != requiredOperatorConfig.Spec.ImagePullSpec {
+		existing.Spec.ImagePullSpec = requiredOperatorConfig.Spec.ImagePullSpec
+		if _, err := client.OpenShiftAPIServerOperatorConfigs().Update(existing); err != nil {
+			panic(err)
+		}
+	}
 }
