502de50f44ccb1f68304c4fece813ec300bfd34b
David Eads <deads@redhat.com>
2019-01-17 12:01:08 -0500

    quick refactor of client

---
+++ pkg/operator/operatorclient/interfaces.go
@@ -0,0 +1,10 @@
+package operatorclient
+
+const (
+	EtcdNamespaceName                     = "kube-system"
+	KubeAPIServerNamespaceName            = "openshift-kube-apiserver"
+	UserSpecifiedGlobalConfigNamespace    = "openshift-config"
+	MachineSpecifiedGlobalConfigNamespace = "openshift-config-managed"
+	OperatorNamespace                     = "openshift-apiserver-operator"
+	TargetNamespaceName                   = "openshift-apiserver"
+)
