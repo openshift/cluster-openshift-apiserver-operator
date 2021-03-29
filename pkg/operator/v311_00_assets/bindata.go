d05f2245e0780860e5f221e1dfffcf067aa9d0e6
Stanislav Laznicka <slaznick@redhat.com>
2021-03-29 13:06:01 +0200

    explicitly allow apiserver pods to write to their root FS

--- pkg/operator/v311_00_assets/bindata.go
+++ pkg/operator/v311_00_assets/bindata.go
@@ -227,6 +227,7 @@ spec:
         # we need to set this to privileged to be able to write audit to /var/log/openshift-apiserver
         securityContext:
           privileged: true
+          readOnlyRootFilesystem: false
         ports:
         - containerPort: 8443
         volumeMounts:
