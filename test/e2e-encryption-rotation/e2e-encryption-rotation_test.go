763105b4bf6d909f8225c0d8858e4d3a0bb1f9b7
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2021-05-19 10:43:25 +0200

    splits the encryption tests
    The current timeout for all encryption test is 4 hours.
    Previously the time needed to roll out a new KAS was 15 minutes, due to a known issue on AWS with the NLB it can take now 22 minutes.
    It means that the tests need ~25 min to make sure that all servers are on the same revision and it is safe to start the test.
    
    Since the rotation scenario needs to enable the encryption first, then wait and check if all resources were migrated, then change the encryption key and check the migration again.
    It means that only these tests will need ~1 hour just for installing the servers.
    
    Thus this PR moves the rotation tests to a separate CI job.
    It will shorten the overall time needed to test encryption but also increase the pass rate of the tests as we have seen them frequently failing to due to a timeout.

--- test/e2e-encryption-rotation/e2e-encryption-rotation_test.go
+++ test/e2e-encryption-rotation/e2e-encryption-rotation_test.go
@@ -1,9 +1,58 @@
 package e2e_encryption_rotation
 
-import "testing"
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/rand"
+
+	"github.com/openshift/cluster-openshift-apiserver-operator/pkg/operator/operatorclient"
+	operatorencryption "github.com/openshift/cluster-openshift-apiserver-operator/test/library/encryption"
+	library "github.com/openshift/library-go/test/library/encryption"
+)
 
 // TestEncryptionRotation first encrypts data with aescbc key
 // then it forces a key rotation by setting the "encyrption.Reason" in the operator's configuration file
 func TestEncryptionRotation(t *testing.T) {
-	t.Log("implement me")
+	ctx := context.TODO()
+	cs := operatorencryption.GetClients(t)
+
+	ns := fmt.Sprintf("test-encryption-on-off-%s", rand.String(4))
+	_, err := cs.KubeClient.CoreV1().Namespaces().Create(ctx, &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: ns}}, metav1.CreateOptions{})
+	require.NoError(t, err)
+	defer cs.KubeClient.CoreV1().Namespaces().Delete(ctx, ns, metav1.DeleteOptions{})
+
+	library.TestEncryptionRotation(t, library.RotationScenario{
+		BasicScenario: library.BasicScenario{
+			Namespace:                       operatorclient.GlobalMachineSpecifiedConfigNamespace,
+			LabelSelector:                   "encryption.apiserver.operator.openshift.io/component" + "=" + operatorclient.TargetNamespace,
+			EncryptionConfigSecretName:      fmt.Sprintf("encryption-config-%s", operatorclient.TargetNamespace),
+			EncryptionConfigSecretNamespace: operatorclient.GlobalMachineSpecifiedConfigNamespace,
+			OperatorNamespace:               operatorclient.OperatorNamespace,
+			TargetGRs:                       operatorencryption.DefaultTargetGRs,
+			AssertFunc:                      operatorencryption.AssertRoutes,
+		},
+		CreateResourceFunc: func(t testing.TB, _ library.ClientSet, _ string) runtime.Object {
+			return operatorencryption.CreateAndStoreRouteOfLife(ctx, t, operatorencryption.GetClients(t), ns)
+		},
+		GetRawResourceFunc: func(t testing.TB, clientSet library.ClientSet, _ string) string {
+			return operatorencryption.GetRawRouteOfLife(t, clientSet, ns)
+		},
+		UnsupportedConfigFunc: func(raw []byte) error {
+			cs := operatorencryption.GetClients(t)
+			apiServerOperator, err := cs.OperatorClient.Get(ctx, "cluster", metav1.GetOptions{})
+			if err != nil {
+				return err
+			}
+			apiServerOperator.Spec.UnsupportedConfigOverrides.Raw = raw
+			_, err = cs.OperatorClient.Update(ctx, apiServerOperator, metav1.UpdateOptions{})
+			return err
+		},
+	})
 }
