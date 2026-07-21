82a9faabcc25214f4a531580a80fe9c1fd35eed7
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2026-07-21 09:23:05 +0200

    pkg/operator/encryptionstatusprovider/provider: with openShiftAPIServerEncryptionStatusProvider

---
+++ pkg/operator/encryptionstatusprovider/provider.go
@@ -0,0 +1,48 @@
+package encryptionstatusprovider
+
+import (
+	"context"
+	"fmt"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/rest"
+
+	operatorv1 "github.com/openshift/api/operator/v1"
+	applyoperatorv1 "github.com/openshift/client-go/operator/applyconfigurations/operator/v1"
+	operatorclient "github.com/openshift/client-go/operator/clientset/versioned"
+
+	"github.com/openshift/library-go/pkg/operator/encryption/kms"
+)
+
+// NewOpenShiftAPIServerEncryptionStatusProviderFromConfig builds a kms.EncryptionStatusProvider for
+// OpenShiftAPIServer/cluster from a rest.Config.
+func NewOpenShiftAPIServerEncryptionStatusProviderFromConfig(restConfig *rest.Config) (kms.EncryptionStatusProvider, error) {
+	client, err := operatorclient.NewForConfig(restConfig)
+	if err != nil {
+		return nil, fmt.Errorf("build operator client: %w", err)
+	}
+	return &openShiftAPIServerEncryptionStatusProvider{client: client}, nil
+}
+
+var _ kms.EncryptionStatusProvider = &openShiftAPIServerEncryptionStatusProvider{}
+
+type openShiftAPIServerEncryptionStatusProvider struct {
+	client *operatorclient.Clientset
+}
+
+func (p *openShiftAPIServerEncryptionStatusProvider) GetKMSEncryptionStatus(ctx context.Context) (*operatorv1.KMSEncryptionStatus, error) {
+	obj, err := p.client.OperatorV1().OpenShiftAPIServers().Get(ctx, "cluster", metav1.GetOptions{})
+	if err != nil {
+		return nil, err
+	}
+	return &obj.Status.EncryptionStatus, nil
+}
+
+func (p *openShiftAPIServerEncryptionStatusProvider) ApplyKMSEncryptionStatus(ctx context.Context, fieldManager string, status *applyoperatorv1.KMSEncryptionStatusApplyConfiguration) error {
+	_, err := p.client.OperatorV1().OpenShiftAPIServers().ApplyStatus(
+		ctx,
+		applyoperatorv1.OpenShiftAPIServer("cluster").WithStatus(applyoperatorv1.OpenShiftAPIServerStatus().WithEncryptionStatus(status)),
+		metav1.ApplyOptions{FieldManager: fieldManager, Force: true},
+	)
+	return err
+}
